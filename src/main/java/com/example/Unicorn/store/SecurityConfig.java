e4085d09724a4a381941e854b6a1e2d116cbfe16
Carlm91 <callemickelson@hotmail.com>
2022-09-07 11:40:43 +0200

    commit 2

--- src/main/java/com/example/Unicorn/store/SecurityConfig.java
+++ src/main/java/com/example/Unicorn/store/SecurityConfig.java
@@ -1,4 +1,47 @@
 package com.example.Unicorn.store;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Autowired
+CustomerRepo customerRepo;
+@Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/unicorns", "/unicorn").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/profile", "/cart").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        for (Customer user: customerRepo.getCustomers()){
+            manager.createUser(User.withDefaultPasswordEncoder().username(user.getUsername()).password(user.getPassword()).roles("USER").build());
+        }
+
+       // manager.createUser(User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("123").roles("ADMIN").build());
+        return manager;
+    }
 }
+
+
+
